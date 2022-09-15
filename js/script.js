f5d4dc91311b9a7febe2ac6194977ce1273c3924
Leerena <leerena7@gmail.com>
2022-09-15 17:32:05 +0300

    fixed overlay

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const modal = document.querySelector('.modal');
 const btnOpenModal = document.querySelector('.btn__submit_srm');
 const btnCloseModal = document.querySelector('.modal__close');
+const overlayModal = document.querySelector('.modal__overlay');
 const modalTitle = document.querySelector('.modal__title');
 const btnModalEditId = document.querySelector('.modal__edit');
 const modalId = document.querySelector('.modal__descr_number');
@@ -15,10 +16,17 @@ const modalTotalSum = document.querySelector('.form__span_sum');
 
 btnOpenModal.addEventListener('click', () => {
     modal.classList.add('active');
+    overlayModal.classList.add('active');
 });
 
 btnCloseModal.addEventListener('click', () => {
     modal.classList.remove('active');
+    overlayModal.classList.remove('active');
+});
+
+overlayModal.addEventListener('click', () => {
+    modal.classList.remove('active');
+    overlayModal.classList.remove('active');
 });
 
 console.log('modalTitle: ', modalTitle);
