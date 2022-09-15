f01239f32e07697eb80344c938bb10d28293fe8d
Leerena <leerena7@gmail.com>
2022-09-15 09:12:28 +0300

    init dev

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productsName = 'Навигационная система Soundmax';
+let productsAmount = 5;
+const productsCategory = 'Техника для дома';
+let productsPrice = 100;
+
+console.log(productsName);
+console.log(productsAmount*productsPrice);
