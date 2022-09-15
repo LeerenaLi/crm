7dd420732dbebde4dbead6b8e3597e5b4a055cfe
Leerena <leerena7@gmail.com>
2022-09-15 11:35:45 +0300

    homework module 5 lesson 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const productsName = 'Навигационная система Soundmax';
-let productsAmount = 5;
-const productsCategory = 'Техника для дома';
-let productsPrice = 100;
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.btn__submit_srm');
+const btnCloseModal = document.querySelector('.modal__close');
+const modalTitle = document.querySelector('.modal__title');
+const btnModalEditId = document.querySelector('.modal__edit');
+const modalId = document.querySelector('.modal__descr_number');
+const modalForm = document.querySelector('#modal-form');
 
-console.log(productsName);
-console.log(productsAmount*productsPrice);
+const modalCheckbox = document.querySelector('.checkbox__input');
+const modalCheckboxBefore = window.getComputedStyle(modalCheckbox, ':before');
+const modalInputDiscount = document.querySelector('.form__input_sale');
+const modalTotalSum = document.querySelector('.form__span_sum');
+
+btnOpenModal.addEventListener('click', () => {
+    modal.classList.add('active');
+});
+
+btnCloseModal.addEventListener('click', () => {
+    modal.classList.remove('active');
+});
+
+console.log('modalTitle: ', modalTitle);
+console.log('btnModalEditId: ', btnModalEditId);
+console.log('modalId: ', modalId);
+console.log('modalForm: ', modalForm);
+console.log('modalCheckboxBefore: ', modalCheckboxBefore);
+console.log('modalInputDiscount: ', modalInputDiscount);
+console.log('modalTotalSum: ', modalTotalSum);
+
+
